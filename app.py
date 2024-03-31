1897d7d61b87c9ac5668441e1e5f7d079caa6dcf
diawando <abduldiawade777@gmail.com>
2024-03-31 11:41:15 +0000

    ajout d'information à la base de donnée

--- app.py
+++ app.py
@@ -1,4 +1,22 @@
-from flask import FLask, render_template, request, jsonify
-import psycopg2
-import psycopg2_binary
+from flask import Flask, render_template, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:lavie@localhost:5432/products"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = "supersecretkey"
+db = SQLAlchemy(app)
+
+class Produit(db.Model):
+    __tablename__ = "produits"
+    
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(70), nullable=False)
+    image = db.Column(db.String(255), nullable=False)
+    category = db.Column(db.String(70), nullable=False)
+    price = db.Column(db.Integer, nullable=False)
+
+
+@app.route('/')
+def inde():
+    return
